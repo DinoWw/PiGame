3f8022ef1e3c36c7cea25b9c88a69ffd40c75387
DinoWw <dinoplecko@yahoo.com>
2024-02-17 20:00:49 +0100

    socket communication established

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const app = require('express')();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);const port = process.env.PORT || 8080;app.get('/', function(req, res) {
-  res.sendfile('index.html');
-});server.listen(port, function() {
+const path = require('path');
+
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 8080;
+
+const options = {
+    root: path.join(__dirname)
+};
+app.get('/', function(req, res) {
+  res.sendFile('./index.html', options);
+});
+
+
+
+
+const gamestate = {
+  founds: []
+};
+
+
+
+io.on('connection', (socket) => {
+  console.log('user connected');  
+
+  // pass gamestate
+  socket.emit("gamestate", gamestate);
+
+  // TODO: validate index
+  // TODO: check if index previously found
+  socket.on("foundMatch", (index) => {
+    console.log("foundMatch: ", gamestate)
+    if(!gamestate.founds.includes(index)) gamestate.founds.push(index);
+    else socket.emit("invalidMatch");
+
+  })
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+})
+
+server.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
